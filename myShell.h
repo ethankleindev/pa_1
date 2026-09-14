3a0156df08fad932e575673379c141b6ef5cd64b
gmancia70 <143858517+gmancia70@users.noreply.github.com>
2026-09-14 09:35:44 -0700

    Update myShell.h
    included windows.h to access threading operations, and also added commandThread so it can be accessed by main.cpp

--- myShell.h
+++ myShell.h
@@ -4,8 +4,10 @@
 #define MYSHELL_H
 #include <vector>
 #include <string>
+#include <windows.h>    //imports the windows OS datat types in order to create and manage threads
 
 std::vector<std::string> parseCommand(const std::string&);
 bool isSupportedCommand(const std::string&);
+DWORD WINAPI commandThread(LPVOID lpParam); //Tells the compiler that commandThread exists and matches the exact signature required by CreateTheread()    
 
 #endif
