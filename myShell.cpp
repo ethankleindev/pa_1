69670713bc6df9786fc86568231368ae6a352047
gmancia70 <143858517+gmancia70@users.noreply.github.com>
2026-09-14 08:52:55 -0700

    Update myShell.cpp
    Hopefully clearing a filter issue with strtok, the delimeter string, to be able to remove "\t" and "t\r\n" this makes it so no matter what operating system runs this code, we will be able to successfully look for valid arguments

--- myShell.cpp
+++ myShell.cpp
@@ -11,11 +11,11 @@ std::vector<std::string> parseCommand(const std::string& input)
     
     //parse and tokenize
     std::vector <std::string> argumentVector;
-    char* token = strtok(buffer, " \t");
-    while (argumentVector.size() < 4 && token != nullptr)
-    {
-        argumentVector.push_back(token);
-        token = strtok(nullptr, " \t");
+    char* token = strtok(buffer, " \t\r\n");                //<--  this is because fgets(); takes the \n line as literal parts of the word 
+    while (argumentVector.size() < 4 && token != nullptr)  // as in if we pass an argument "dir" fgets(); will read it as "dir\n" literally
+    {                                                      //in the buffer. We want to make sure that strtok will remove \r and \n. \r
+        argumentVector.push_back(token);                  // makes it so the cursor moves to the beginning of a line, and \n moves it down
+        token = strtok(nullptr, " \t");                  // a row. 
     }
     return argumentVector;
 }
