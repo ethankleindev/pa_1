ccb2457f695dab22a0f29580f4c1cf1f768e2d88
gmancia70 <143858517+gmancia70@users.noreply.github.com>
2026-09-14 11:05:09 -0700

    Added commandThread, forgot to implement it lol

--- myShell.cpp
+++ myShell.cpp
@@ -17,7 +17,7 @@ std::vector<std::string> parseCommand(const std::string& input)
     while (argumentVector.size() < 4 && token != nullptr)  // as in if we pass an argument "dir" fgets(); will read it as "dir\n" literally
     {                                                      //in the buffer. We want to make sure that strtok will remove \r and \n. \r
         argumentVector.push_back(token);                  // makes it so the cursor moves to the beginning of a line, and \n moves it down
-        token = strtok(nullptr, " \t");                  // a row. In certain OS's it will send "dir\r\n" and this will make it filter it out 
+        token = strtok(nullptr, " \t\r\n");              // a row. In certain OS's it will send "dir\r\n" and this will make it filter it out 
     }
     return argumentVector;
 }
@@ -35,3 +35,24 @@ bool isSupportedCommand(const std::string& command)
     }
     return false;   
 }
+
+DWORD WINAPI commandThread(LPVOID lpParam)
+{
+    auto args = static_cast<std::vector<std::string>*>(lpParam);    //copies all the information from lpParam and stores each word into a vector, it does this with a pointer to access each individual cell
+    if (args != nullptr && !args->empty())
+    {
+        std::string fullCommand = "";
+        for (size_t i = 0; i < args-> size(); ++i)      // gets each individual character in the lpParam and copies them to store the command
+            {
+                fullCommand += (*args)[i];
+                if(i < args-> size() - 1)
+                {
+                    fullCommand += " ";
+                }
+            }                               
+        system(fullCommand.c_str());       //Runs it on the child thread while main.cpp waits
+    }
+    delete args;              //Prevents data leaks
+    return 0;
+}
+
